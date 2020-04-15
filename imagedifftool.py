3569b3e0f329778a45aa102e4e4321903c09dc67
Marinelle Rowe <marinelle.rowe@inl.gov>
2020-04-15 16:05:06 -0600

    ./imagedifftool.py

---
+++ imagedifftool.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse, os, sys, filetype
+
+parser = argparse.ArgumentParser(description="Image comparison tool")
+parser.add_argument("img", type=str, nargs='+', help="display filepath to image")
+
+args = parser.parse_args()
+
+for image in args.img:
+  if not os.path.exists(image):
+    print('%s Does not exist' % (image))
+    sys.exit(1)
+  else:
+    my_file = filetype.guess(image)
+    if not my_file == 'image/jpg':
+      print('%s is not a jpeg' % (image))
+      sys.exit(1)
